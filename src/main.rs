1524945c2ad050e0290b6fc450d992634a729b27
Augusto Dias <augusto@stilingue.com.br>
2019-06-12 03:46:45 -0300

    Implementando propriedades para teste

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,54 @@
 mod core;
 
-fn tim_sort(v: &Vec<i32>) -> Vec<i32> {
-	let mut vector = v.clone();
-	vector.sort();
-	vector
+fn sort_vec(v: &Vec<i32>) -> Vec<i32> {
+    let mut vector = v.clone();
+    vector.sort();
+    vector
 }
 
-// Amazing O(1) Sorting
-fn ao1_sort(v: &Vec<i32>) -> Vec<i32> {
-	v.clone()
+fn vec_sort_property(test_case: &Vec<i32>) -> bool {
+    let sorted_vec = sort_vec(test_case);
+    for i in 1..sorted_vec.len() {
+        if sorted_vec.get(i-1) > sorted_vec.get(i) {
+            return false
+        }
+    }
+    true
 }
 
-fn sort_check<F>(f: &'static F) -> impl Fn(&Vec<i32>) -> bool where 
-	F: Fn(&Vec<i32>) -> Vec<i32> + Sized
-{
-	move |v| {
-		let sorted = f(v);
+fn vec_insertion_size_property(test_case: &(Vec<i32>, i32)) -> bool {
+    let (immutable_vector, number) = test_case;
+    let mut vector = immutable_vector.clone();
+    let old_length = vector.len();
+    vector.push(*number);
+    if vector.len() != old_length + 1 {
+        return false
+    }
+    true
+}
 
-		println!("Sorted vector: {:?}", sorted);
-		
-		for i in 1..sorted.len() {
-			if sorted.get(i-1) > sorted.get(i) {return false}
-		}
-		true
-	}
+fn vec_insertion_property(test_case: &(Vec<i32>, i32)) -> bool {
+    let (immutable_vector, number) = test_case;
+    let mut vector = immutable_vector.clone();
+    vector.push(*number);
+    vector.contains(number)
+}
+
+fn vec_removal_size_property(test_case: &Vec<i32>) -> bool {
+    let mut vector = test_case.clone();
+    let old_length = vector.len();
+    vector.pop();
+    if vector.len() != old_length - 1 {
+        return false
+    }
+    true
 }
 
 fn main () {
-	let tim_sort_prop = sort_check(&tim_sort);
-	let ao1_sort_prop = sort_check(&ao1_sort);
-	
-	let mut test = core::QuickCheck::new();
-	
-	test.set_config(100, 100);
-
-	println!("\n\n==== timsort test ====\n");
-	test.run::<Vec<i32>>(&tim_sort_prop);
-
-	println!("\n\n==== ao1sort test ====\n");
-	test.run::<Vec<i32>>(&ao1_sort_prop);
+    let mut test = core::QuickCheck::new();
+    test.set_config(10, 100);
+    test.run::<Vec<i32>>(&vec_sort_property);
+    test.run::<(Vec<i32>, i32)>(&vec_insertion_property);
+    test.run::<(Vec<i32>, i32)>(&vec_insertion_size_property);
+    test.run::<Vec<i32>>(&vec_removal_size_property);
 }
